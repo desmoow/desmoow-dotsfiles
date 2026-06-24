63e7219ac37102d22bc1e3106fe70806a68e2dd4
desmoow <desmoow@gmail.com>
2026-06-24 14:39:29 +0200

    Create alpha.lua

---
+++ nvim/lua/plugins/alpha.lua
@@ -0,0 +1,28 @@
+local alpha = require('alpha')
+local dashboard = require("alpha.themes.dashboard")
+dashboard.section.header.val = {
+  "██████╗ ███████╗███████╗███╗   ███╗ ██████╗  ██████╗ ██╗    ██╗",
+  "██╔══██╗██╔════╝██╔════╝████╗ ████║██╔═══██╗██╔═══██╗██║    ██║",
+  "██║  ██║█████╗  ███████╗██╔████╔██║██║   ██║██║   ██║██║ █╗ ██║",
+  "██║  ██║██╔══╝  ╚════██║██║╚██╔╝██║██║   ██║██║   ██║██║███╗██║",
+  "██████╔╝███████╗███████║██║ ╚═╝ ██║╚██████╔╝╚██████╔╝╚███╔███╔╝",
+  "╚═════╝ ╚══════╝╚══════╝╚═╝     ╚═╝ ╚═════╝  ╚═════╝  ╚══╝╚══╝",
+}
+
+dashboard.section.buttons.val = {
+	dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+	dashboard.button("f", "󰍉  Find file", ":lua require('fzf-lua').files() <CR>"),
+	dashboard.button("r", "  Browse src", ":e ~/.local/src/<CR>"),
+	dashboard.button("c", "  Config", ":e ~/.config/nvim/<CR>"),
+	dashboard.button("m", "  Mappings", ":e ~/.config/nvim/lua/config/mappings.lua<CR>"),
+	dashboard.button("p", "  Plugins", ":PlugInstall<CR>"),
+	dashboard.button("q", "󰅙  Quit", ":q!<CR>"),
+}
+
+dashboard.section.footer.val = function()
+  return vim.g.startup_time_ms or "[[  ]]"
+end
+
+dashboard.section.buttons.opts.hl = "Keyword"
+dashboard.opts.opts.noautocmd = true
+alpha.setup(dashboard.opts)
