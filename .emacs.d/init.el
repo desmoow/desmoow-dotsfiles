2af4aa46305ade2fedcc5b65613e71ce065a5344
desmoow <desmoow@gmail.com>
2026-07-10 15:06:04 +0200

    Update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,14 @@
+(add-to-list 'load-path "~/.emacs.d/lisp")
+(setq custom-file "~/.emacs.d/lisp")
+
+(add-to-list 'custom-theme-load-path
+             "~/.emacs.d/lisp/mindre-theme")
+
+(setq mindre-use-more-bold nil
+      mindre-use-faded-lisp-parens t)
+
+(load-theme 'mindre t)
+
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
@@ -15,17 +26,7 @@
 (ido-mode 1)
 (electric-pair-mode 1)
 
-(global-set-key (kbd "M-m") #'find-file)
-(global-set-key (kbd "M-s") #'save-buffer)
-
-(add-to-list 'default-frame-alist '(font . "IosevkaTerm-20"))
-(custom-set-variables
- '(custom-enabled-themes '(kaolin-bubblegum))
- '(custom-safe-themes
-   '("f7a004e3a1f920d79a02988fd20a9af9f5cedb1491918e1ef247bf7481cbe93b"
-  default))
- '(package-selected-packages '(doric-themes gotham-theme kaolin-themes moe-theme)))
-(custom-set-faces )
+(add-to-list 'default-frame-alist '(font . "Spleen-17"))
 
 (require 'package)
 
@@ -35,6 +36,34 @@
         ("melpa" . "https://melpa.org/packages/")
 	("org" . "https://orgmode.org/elpa/")))
 
+
 (package-initialize)
 (unless package-archive-contents
   (package-refresh-contents))
+
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(use-package evil
+  :ensure t
+  :init
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-i-jump nil)
+
+  :config
+  (evil-mode 1))
+
+(use-package hima-theme
+  :ensure t
+  :config 
+  ;;(load-theme 'hima t)
+  )
+
+(use-package vertico
+  :ensure t
+  :config
+  (vertico-mode 1))
